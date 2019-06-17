5c8559a6655f3159ed59ab8af3f9cb5913df9562
bhgonzales <bhgonzales@umsa.bo>
2019-06-17 09:33:43 -0500

    mandar

--- src/app/layout/menu-main/menu-main.component.ts
+++ src/app/layout/menu-main/menu-main.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-declare var $:any;
+declare var $: any;
 
 @Component({
   selector: 'app-menu-main',
@@ -7,26 +7,25 @@ declare var $:any;
   styles: []
 })
 export class MenuMainComponent implements OnInit {
-  listaMenu : string[]=[];
+  listaMenu: string[] = [];
   constructor() { }
 
   ngOnInit() {
     this.listarMenu();
-    //this.jquery_init();
+    this.jquery_init();
   }
-  listarMenu(){
+  listarMenu() {
     console.log("jola");
     this.listaMenu.push("menu1");
     this.listaMenu.push("menu2");
     this.listaMenu.push("menu3");
     this.listaMenu.push("menu4");
   }
-  public jquery_init(event):void{
-    console.log("ya cargue mi modulo")
-    console.log(event);
-    $('[name="menu-item-head"]').click(function(){
-      console.log("golas")
-        $('[name="sub-menu-item"]').toggleClass("is-hidden");
+  public jquery_init(): void {
+    console.log("hola");
+    //$('.ui.sidebar').sidebar('toggle');
+    $('a.sidebar-toggle').click(function () {
+      $('#sidebar').sidebar('toggle');
     });
   }
 }
